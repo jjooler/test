ef9d816093e9fa97e96aa316baec03ed25e93aa7
Julian Kuy <41023108+jjooler@users.noreply.github.com>
2018-07-12 02:20:37 -0400

    Client-Server Two-Way Messaging

--- src/test/Client.java
+++ src/test/Client.java
@@ -4,18 +4,83 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Client 
 {
-	public static void main(String[] args) throws UnknownHostException, IOException
-	{
-		Socket socket = new Socket("127.0.0.1",1234);
-		PrintStream stream = new PrintStream(socket.getOutputStream());
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		
-		Scanner scan = new Scanner(socket.getInputStream());
-		Scanner input = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		
-		stream.println(input.nextLine());
+		System.out.print("Enter an IP: ");
+		String IP = scan.nextLine();
+		
+		Socket socket = new Socket(IP,1234);
+		String user = socket.getLocalAddress().getHostAddress();
+		
+		Thread t1 = new Thread(new Runnable() {
+		    @Override
+		    public void run() {
+		    	PrintStream output = null;
+		    	String message = "";
+		    	
+			    	while(true)
+			    	{			
+			    		try {
+			    			output = new PrintStream(socket.getOutputStream());
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						
+			    		message = user + ": " + scan.nextLine();
+			    		
+			    		if(message.equalsIgnoreCase(user + ": quit") || socket.isClosed())
+			    		{
+			    			System.out.println("Disconnected from server.");
+			    			break;
+			    		}
+			    		else
+			    			output.println(message);
+			    	}
+			
+			    	output.close();
+			    	scan.close();
+			    	
+			    	try {
+						socket.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		    }
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+		    @Override
+		    public void run() {
+		    	Scanner input = null;
+				
+					while(t1.isAlive()) {
+						try {
+							input = new Scanner(socket.getInputStream());
+						} catch (IOException e) {
+						}
+						
+						try {
+							System.out.println(input.nextLine());
+						} catch(NoSuchElementException ee) {
+							if(t1.isAlive())
+								System.out.println("Server offline!");
+							break;
+						}
+					}
+
+					input.close();
+		    }
+		});
+		
+		t1.start();
+		t2.start();
 	}
 }
--- src/test/Server.java
+++ src/test/Server.java
@@ -1,19 +1,89 @@
 package test;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Server {
 	
-	public static void main(String[] args) throws IOException
-	{
+	public static void main(String[] args) throws IOException {
 		ServerSocket socket = new ServerSocket(1234);
+		System.out.println("Waiting for client...");
 		Socket accept = socket.accept();
-		Scanner scan = new Scanner(accept.getInputStream());
+		String server = accept.getLocalAddress().getHostAddress();
+		String user = accept.getInetAddress().getHostAddress();
 		
-		System.out.println(scan.nextLine());
+		Scanner scan = new Scanner(System.in);
+		
+		Thread t1 = new Thread(new Runnable() {
+		    @Override
+		    public void run() {
+		    	PrintStream output = null;
+		    	
+				try {
+					output = new PrintStream(accept.getOutputStream());
+				} catch (IOException e2) {
+					e2.printStackTrace();
+				}
+				
+				System.out.println("Client connected from: " + user);
+				output.println("Client connected from: " + user);
+				
+		    	String message = "";
+		    	
+			    	while(true)
+			    	{	
+			    		message = server + ": " + scan.nextLine();
+			    		
+			    		if(message.equalsIgnoreCase(server + ": quit") || accept.isClosed()) {
+			    			System.out.println("Server closed.");
+			    			break;
+			    		}
+			    		else
+			    			output.println(message);
+			    	}
+			    	
+			    	output.close();
+			    	scan.close();
+			    	
+			    	try {
+						socket.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		    }
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+		    @Override
+		    public void run() {
+		    	Scanner input = null;
+				
+					while(t1.isAlive()) {
+						try {
+							input = new Scanner(accept.getInputStream());
+						} catch (IOException e) {
+							break;
+						}
+						
+						try {
+							System.out.println(input.nextLine());
+						} catch(NoSuchElementException ee) {
+							if(t1.isAlive())
+								System.out.println("Client has disconnected.");
+							break;
+						}
+					}
+					
+					input.close();
+		    }
+		});
+		
+		t1.start();
+		t2.start();
 	}
-	
 }
